01cdb72127daa656454e2e11bcd4f00a066acc69
Lucas Duarte <lucas.duarte@inspira.com.br>
2017-07-05 19:32:49 -0300

    [SITE] Save on ajax
    by [aripmav1896@gmail.com] in [2011-10-23 00:00:40 (-02:00)]

---
+++ Presentation/Assets/js/Author/SaveChangeColor.js
@@ -0,0 +1,15 @@
+﻿function SaveBegin() {
+    $("#save").removeClass("greenSave");
+    $("#save").removeClass("redSave");
+    $("#save").addClass("yellowSave");
+}
+
+function SaveSuccess() {
+    $("#save").removeClass("yellowSave");
+    $("#save").addClass("greenSave");
+}
+
+function SaveFailure() {
+    $("#save").removeClass("yellowSave");
+    $("#save").addClass("redSave");
+}
