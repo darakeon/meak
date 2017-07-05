d8c792905a25c1d0a1f519b26393f879648c7ae9
Lucas Duarte <lucas.duarte@inspira.com.br>
2017-07-05 20:29:53 -0300

    [SITE] Added countdown
    by [Dara Keon (desktop)] in [2014-02-01 23:16:22 (-02:00)]

--- Structure/Helpers/Config.cs
+++ Structure/Helpers/Config.cs
@@ -11,5 +11,7 @@ namespace Structure.Helpers
         public static String StoriesPath = ConfigurationManager.AppSettings["Stories"];
         public static String MessagesPath = ConfigurationManager.AppSettings["Messages"];
 
+        public static DateTime CountdownStart = DateTime.Parse(ConfigurationManager.AppSettings["CountdownStart"]);
+        public static Int32 CountdownFrequency = Int32.Parse(ConfigurationManager.AppSettings["CountdownFrequency"]);
     }
 }
---
+++ Structure/Helpers/Countdown.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Structure.Helpers
+{
+    public class Countdown
+    {
+        public static String GetTimeLeft()
+        {
+            var allTime = DateTime.UtcNow - Config.CountdownStart;
+
+            var timeLeft = Config.CountdownFrequency -
+                (allTime.TotalDays % Config.CountdownFrequency);
+
+            var positions = Math.Ceiling(Math.Log10(Config.CountdownFrequency));
+            var format = Math.Pow(10, positions).ToString().Replace("1", "");
+
+            return Math.Ceiling(timeLeft).ToString(format);
+        }
+
+    }
+}
