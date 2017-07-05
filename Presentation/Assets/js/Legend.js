8368fc12b17f36e1cd7a55c03a0c5a25090768a9
Lucas Duarte <lucas.duarte@inspira.com.br>
2017-07-05 19:35:08 -0300

    [SITE] Border radius
    by [handhara] in [2011-10-27 19:17:06 (-02:00)]

--- Presentation/Assets/js/Legend.js
+++ Presentation/Assets/js/Legend.js
@@ -2,8 +2,11 @@ var legendOpened = true;
 
 function AnimateLegend() {
 
+    $("#legend").toggleClass("lightback");
+
     $("#legend .header").toggleClass("lightback");
     $("#legend .header").toggleClass("darkback");
+
     $("#legend .body").toggle();
     
     legendOpened = !legendOpened;
