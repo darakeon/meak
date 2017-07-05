ae36fda57382bced523a3bcff9373940518e746a
Lucas Duarte <lucas.duarte@inspira.com.br>
2017-07-05 20:13:39 -0300

    [SITE] Last ajusts in legend
    by [aripmav1896@gmail.com] in [2012-08-26 22:53:24 (-03:00)]

--- Presentation/Assets/js/Legend.js
+++ Presentation/Assets/js/Legend.js
@@ -1,12 +1,6 @@
 var legendOpened = false;
 
 function AnimateLegend() {
-
-    $("#legend").toggleClass("lightback");
-
-    $("#legend .header").toggleClass("lightback");
-    $("#legend .header").toggleClass("darkback");
-
     $("#legend .body").toggle();
     
     legendOpened = !legendOpened;
