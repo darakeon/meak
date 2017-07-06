bb62cd8fb3b1ff86a3af677f5e5345147e4be0e2
Lucas Duarte <lucas.duarte@inspira.com.br>
2017-07-05 21:03:16 -0300

    [SITE] Save with ctrl for summary and title
    by [Dara Keon (Ísis)] in [2015-12-28 19:42:22 (-02:00)]

--- Site/Presentation/Assets/js/Author/Inputs.js
+++ Site/Presentation/Assets/js/Author/Inputs.js
@@ -2,12 +2,21 @@ function SaveButton() {
     $("input").keydown(function () {
         NeedSaveAgain($(this).closest("form").attr("id"));
     });
+
+    $(".titleChange input, .titleChange textarea").focus(function () {
+    	saveTitle = true;
+    });
+
+    $(".titleChange input, .titleChange textarea").blur(function () {
+    	saveTitle = false;
+    });
 }
 
 
 
 var pressedCtrl = false;
 var pressedShift = false;
+var saveTitle = false;
 var canSubmit = true;
 
 function SaveKey() {
@@ -22,7 +31,7 @@ function SaveKey() {
 
             if (canSubmit) {
                 if (pressedCtrl && e.which == 89) {
-                    SaveCurrentScene();
+                    SaveCurrentForm();
                     pressedCtrl = false;
                 }
             }
@@ -40,6 +49,14 @@ function Pressed(key, set) {
         case (17): pressedCtrl = set; break;
         case (16): pressedShift = set; break;
     }
+}
+
+function SaveCurrentForm() {
+	if (saveTitle) {
+		$(".titleChange").parent("form").submit();
+	} else {
+		SaveCurrentScene();
+	}
 }
 
 function SaveCurrentScene() {
