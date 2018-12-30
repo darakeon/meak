ff4409928946afa704855fc8cf9771d041d9e7cf
Dara Keon <lc.sales.duarte@gmail.com>
2018-12-30 19:08:12 -0200

    [SITE] assembly-info: unify version

---
+++ Site/Structure/Properties/GeneralAssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Version = Structure.Version;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Keon")]
+[assembly: AssemblyProduct("Keon.Stories")]
+[assembly: AssemblyCopyright("Copyright © Keon 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion(Version.Name)]
+[assembly: AssemblyFileVersion(Version.Name)]
---
+++ Site/Structure/Version.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Structure
+{
+	public class Version
+	{
+		public const String Name = "1.0.0.0";
+	}
+}
