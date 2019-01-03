041c18f928b06b9869bcb11b3855999de721dea9
Dara Keon <lc.sales.duarte@gmail.com>
2019-01-03 18:51:37 -0200

    [SITE] title: remove unnecessary field

--- Site/Structure/Entities/Json/SummaryPart.cs
+++ Site/Structure/Entities/Json/SummaryPart.cs
@@ -10,15 +10,9 @@ namespace Structure.Entities.Json
 		public String Last { get; set; }
 		public DateTime Publish { get; set; }
 
-		public String Portuguese { get; set; }
-		public String English { get; set; }
 		public Int16 Page { get; set; }
 
-		internal String Title
-		{
-			get => Portuguese;
-			set => Portuguese = value;
-		}
+		public String Title { get; set; }
 
 		public String Summary { get; set; }
 	}
