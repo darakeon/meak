a56af243c4726db8e64808925721d55b433b2940
Dara Keon <lc.sales.duarte@gmail.com>
2021-01-25 19:55:00 -0300

    [SITE] Add docker compose to deploy site

--- docker/run.bat
+++ docker/run.bat
@@ -6,6 +6,9 @@ docker build . -f mebrak.dockerfile -t darakeon/mebrak
 del mebrak.dockerfile > NUL
 cd docker
 
-docker rm -f mebrak > NUL 2> NUL
-docker run --name mebrak -p 80:80 -p 443:443 -it darakeon/mebrak
+if "%1" == "start" (
+	docker rm -f mebrak > NUL 2> NUL
+	docker run --name mebrak -p 80:80 -p 443:443 -it darakeon/mebrak
+)
+
 docker push darakeon/mebrak
