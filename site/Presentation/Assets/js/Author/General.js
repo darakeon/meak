33b4c995f33b11117da1f25d8d8fd75268451b6a
Dara Keon <lc.sales.duarte@gmail.com>
2021-10-19 19:16:53 -0300

    site: adjust main info box

--- site/Presentation/Assets/js/Author/General.js
+++ site/Presentation/Assets/js/Author/General.js
@@ -2,5 +2,18 @@ $(document).ready(function () {
 	SaveKey();
 	SaveButton();
 
-	$("form")[0].reset();
+	$('form')[0].reset();
+
+	$('#main-info .toggle').click(function () {
+		const form = $('#main-info form');
+
+		if (form.is(":visible")) {
+			$('#main-info form').hide()
+			$(this).html('&#129090;')
+		} else {
+			$('#main-info form').show()
+			$(this).html('&#129088;')
+		}
+	})
 });
+
