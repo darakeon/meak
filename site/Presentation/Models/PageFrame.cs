81e9f2213f408c5fc637b5febb329fd28555577e
Dara Keon <lc.sales.duarte@gmail.com>
2021-10-23 14:42:38 -0300

    print: adjust first pages

---
+++ site/Presentation/Models/PageFrame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Structure.Entities.System;
+
+namespace Presentation.Models
+{
+	public class PageFrame
+	{
+		public PageFrame(Episode episode, Int32 page)
+		{
+			Series = "MEAK";
+			Code = $"{episode.Season}{episode}";
+			Title = episode.Title;
+			Page = page;
+		}
+
+		public String Series { get; }
+		public String Code { get; }
+		public String Title { get; }
+		public Int32 Page { get; }
+	}
+}
